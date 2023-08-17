09c54ce373a2e854c05a4791cb1780c2a0e4cd1a
Spas Z. Spasov <spas.z.spasov@metalevel.tech>
2023-08-17 13:51:51 +0300

    feat: initial setup of the project

--- next.config.js
+++ next.config.js
@@ -1,4 +1,37 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	reactStrictMode: true,
+	images: {
+		// https://nextjs.org/docs/messages/next-image-unconfigured-host
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "lh3.googleusercontent.com",
+				port: "",
+				pathname: "/**",
+			},
+			{
+				protocol: "https",
+				hostname: "avatars.githubusercontent.com",
+				port: "",
+				pathname: "/**",
+			},
+		],
+	},
+	async headers() {
+		return [
+			{
+				source: "/:all*(svg|jpg|png|webp|webm|mkv|avi|mp4)",
+				locale: false,
+				headers: [
+					{
+						key: "Cache-Control",
+						value: "public, max-age=604800, s-maxage=604800, must-revalidate",
+					},
+				],
+			},
+		];
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
