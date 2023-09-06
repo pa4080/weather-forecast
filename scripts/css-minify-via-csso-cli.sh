64e48954311762cdcd14fc42960e06a5dce74cc7
Spas Z. Spasov <spas.z.spasov@metalevel.tech>
2023-09-06 14:39:54 +0300

    fix: repository languages by using .gitattributes

--- scripts/css-minify-via-csso-cli.sh
+++ scripts/css-minify-via-csso-cli.sh
@@ -5,7 +5,7 @@
 # @see https://www.npmjs.com/package/csso
 # @see https://github.com/css/csso-cli
 
-cd "public/assets/css/weather-icons"
+cd "public/css/weather-icons"
 for file in *{icons,wind}.css; do
   echo -e "\nMinifying $file"
   npx csso --input "$file" --output "${file%.css}.min.css"
